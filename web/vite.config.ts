36b390324eac95353e9dbdc0d1e01b0d619bfc25
lategardener <djolemarc@cy-tech.fr>
2026-01-13 12:26:53 +0100

    update base path in Vite configuration for GitHub repository

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Le "base" doit correspondre au nom de votre dépôt GitHub entre slashs
+  base: '/cy-weather/',
 })
